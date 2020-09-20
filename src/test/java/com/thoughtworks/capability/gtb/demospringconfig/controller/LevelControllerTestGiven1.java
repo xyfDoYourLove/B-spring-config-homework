5fa9f82781b254ec2b51ef479c08b4490a3993e8
DoYourLove <xyf15620168540@163.com>
2020-09-20 22:46:37 +0800

    [XiaoYiFan] feat : finish problem2 with set properties

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/controller/LevelControllerTestGiven1.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/controller/LevelControllerTestGiven1.java
@@ -11,7 +11,7 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@SpringBootTest
+@SpringBootTest(properties = {"levelNumber=1"})
 @AutoConfigureMockMvc
 class LevelControllerTestGiven1 {
 
