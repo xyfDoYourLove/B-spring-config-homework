ca8f13dddf69b47d8b4222d0dd86f0fb8fa0ad96
DoYourLove <xyf15620168540@163.com>
2020-09-20 21:10:56 +0800

    [XiaoYiFan] feat : and basic test method

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/controller/LevelControllerTest.java
@@ -0,0 +1,40 @@
+package com.thoughtworks.capability.gtb.demospringconfig.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LevelControllerTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Test
+  void should_return_basic_when_get_level_given_level_less_than_1() throws Exception {
+    mockMvc.perform(get("/level"))
+            .andExpect(jsonPath("$", is("basic")));
+  }
+
+  @Test
+  void should_return_basic_when_get_level_given_level_equals_1() throws Exception {
+    mockMvc.perform(get("/level"))
+            .andExpect(jsonPath("$", is("advanced")));
+  }
+
+  @Test
+  void should_return_advanced_when_get_level_given_level_greater_than_1() throws Exception {
+    mockMvc.perform(get("/level"))
+            .andExpect(jsonPath("$", is("advanced")));
+  }
+
+
+}
